b908336ecbae1470eb5e35888dc37478d83fb956
Cauan Cabral <cauan@radig.com.br>
2010-08-20 00:21:57 -0400

    Validador minimamente funcional

--- models/behaviors/pass_validator.php
+++ models/behaviors/pass_validator.php
@@ -24,10 +24,13 @@ class PassValidatorBehavior extends ModelBehavior
 		$this->settings = array(
 			'fields' => array(
 				'password' => 'password',
-				'confirm' => 'passoword_confirm'
+				'confirm' => 'password_confirm'
 			),
+			'haveConfirm' => true,
+			'isSecurityPassword' => true,
 			'minLength' => 4,
-			'allowEmpty' => false
+			'allowEmpty' => false,
+			'unsetInFailure' => true
 		);
 		
 		if(!empty($config) && is_array($config))
@@ -44,59 +47,104 @@ class PassValidatorBehavior extends ModelBehavior
 	
 	public function beforeValidate(&$model)
 	{
+		$pass = $this->model->data[$this->model->name][$this->settings['fields']['password']];
+		
+		if($this->settings['haveConfirm'])
+		{
+			$confirm = $this->model->data[$this->model->name][$this->settings['fields']['confirm']];
+		}
+		else
+		{
+			$confirm = null;
+		}
+		
+		$success = $this->isValidPassword($pass, $confirm);
+		
+		if($success !== true)
+		{
+			$this->model->validationErrors = array_merge($this->model->validationErrors, $success);
+			
+			if($this->settings['unsetInFailure'])
+			{
+				unset($this->model->data[$this->model->name][$this->settings['fields']['password']]);
+				unset($this->model->data[$this->model->name][$this->settings['fields']['confirm']]);
+			}
+		}
+		
 		parent::beforeValidate($model);
 		
-		return $this->isValidPassword($this->model->data[$this->model->name]['password'], $this->model->data[$this->model->name]['password_confirm']);
+		return true;
 	}
 	
-	public function isValidPassword(&$pass, &$confirm)
+	public function isValidPassword($pass, $confirm = null)
 	{
-		// caso seja uma atualização do registro (onde a senha não pode ser alterada)
-		if(isset($this->model->data[$this->model->name]['id']) && !isset($this->model->data[$this->name]['password_confirm']))
+		$errors = array();
+		
+		// caso seja uma atualização do registro (onde a senha já está em hash)
+		if(isset($this->model->data[$this->model->name]['id']))
 		{
+			// saí da validação
 			return true;
 		}
 		
-		// campo esta vazio e isso é permitido
-		if(empty($pass) && $this->settings['allowEmpty'])
+		// caso seja permitido não preencher o campo de senha
+		if($this->settings['allowEmpty'])
+		{
+			if(empty($pass))
+			{
+				return true;
+			}
+		}
+		//caso não seja permitido
+		else
+		{
+			if(empty($pass))
+			{
+				$errors[$this->settings['fields']['password']] = $this->settings['errors']['required'];
+			}
+		}
+		
+		
+		// validações que dependem do campo de confirmação
+		if($this->settings['haveConfirm'])
+		{
+			// campo de confirmação esta vazio
+			if(empty($confirm))
+			{
+				$errors[$this->settings['fields']['confirm']] = $this->settings['errors']['confirm'];
+			}
+					
+			// valida o tamanho da senha, depende de acesso ao campo de confirmação
+			if(mb_strlen($confirm) < $this->settings['minLength'])
+			{
+				$errors[$this->settings['fields']['confirm']] = $this->settings['errors']['minLength'];
+			}
+			
+			$hash = Security::hash($confirm, null, true);
+			
+			// valida se o hash da senha é o mesmo da confirmação
+			if($pass != $hash)
+			{
+				$errors[$this->settings['fields']['confirm']] = $this->settings['errors']['confirm'];
+			}
+		}
+		// caso o campo de senha não venha em hash, é possível usar o próprio campo
+		else if(!$this->settings['isSecurityPassword'])
+		{
+			// valida o tamanho da senha, depende de acesso ao campo de confirmação
+			if(mb_strlen($pass) < $this->settings['minLength'])
+			{
+				$errors[$this->settings['fields']['password']] = $this->settings['errors']['minLength'];
+			}
+		}
+		
+		if(empty($errors))
 		{
 			return true;
 		}
-		
-		// campo esta vazio mas isso não é permitido
-		if(empty($pass) && !$this->settings['allowEmpty'])
+		else
 		{
-			$this->validationErrors[$this->settings['fields']['password']] = $this->settings['errors']['required'];
-			
-			return false;
+			return $errors;
 		}
-		
-		// campo de confirmação esta vazio
-		if(empty($confirm))
-		{
-			$this->validationErrors[$this->settings['fields']['confirm']] = $this->settings['errors']['confirm'];
-			
-			return false;
-		}
-				
-		// valida o tamanho da senha, pela sua confirmação
-		if(mb_strlen($confirm) < $this->settings['minLength'])
-		{
-			$this->validationErrors[$this->settings['fields']['confirm']] = $this->settings['errors']['minLength'];
-			
-			return false;
-		}
-		
-		$hash = Security::hash($confirm, null, true);
-		
-		// valida se o hash da senha é o mesmo da confirmação
-		if($passwd != $hash)
-		{
-			$this->validationErrors[$this->settings['fields']['confirm']] = $this->settings['errors']['confirm'];
-			
-			return false;
-		}
-		
-		return true;
 	}
 }
