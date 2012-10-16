c6c6a65afd38051281ca1d4113b7deb18d3d40a4
Cauan Cabral <cauan@radig.com.br>
2012-10-16 09:49:43 -0400

    Corrigindo notices do PHP 5.4 em modo estrito

--- Model/Behavior/PassValidatorBehavior.php
+++ Model/Behavior/PassValidatorBehavior.php
@@ -1,17 +1,17 @@
 <?php
-/** 
+/**
  * Behavior to make common password validation
- * 
+ *
  * Code comments in brazilian portuguese.
  * -----
  * Behavior que efetua validações comuns em senhas
- * 
- * PHP version 5
- * 
+ *
+ * PHP version 5.3
+ *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
- * 
- * @copyright 2010-2011, Radig - Soluções em TI, www.radig.com.br
+ *
+ * @copyright 2010-2012, Radig - Soluções em TI, www.radig.com.br
  * @link http://www.radig.com.br
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  *
@@ -26,21 +26,21 @@ class PassValidatorBehavior extends ModelBehavior
 {
 	/**
 	 * Referência para o modelo ligado ao behavior
-	 * 
+	 *
 	 * @var Model
 	 */
 	protected $model;
-	
+
 	/**
 	 * Configuração do behavior
 	 * Mescla configurações padrões com as fornecidas pelo usuário
-	 * 
+	 *
 	 * @see ModelBehavior::setup()
 	 */
-	public function setup(&$model, $config = array())
+	public function setup(Model $model, $config = array())
 	{
-		$this->model =& $model;
-		
+		$this->model = $model;
+
 		$this->settings = array(
 			'fields' => array(
 				'password' => 'password',
@@ -55,12 +55,12 @@ class PassValidatorBehavior extends ModelBehavior
 			'allowEmpty' => false,
 			'unsetInFailure' => true
 		);
-		
+
 		if(!empty($config) && is_array($config))
 		{
 			$this->settings = Set::merge($this->settings, $config);
 		}
-		
+
 		$this->settings['errors'] = array(
 			'required' => __('Campo obrigatório'),
 			'minLength' => __(sprintf('Insira pelo menos %d caracteres', $this->settings['minLength'])),
@@ -70,15 +70,15 @@ class PassValidatorBehavior extends ModelBehavior
 			'minSpecialChars' => __(sprintf('A senha deve ter pelo menos %d caracteres especiais', $this->settings['minSpecialChars']))
 		);
 	}
-	
+
 	/**
 	 * Validação é feita no callback beforeValidate()
-	 * 
+	 *
 	 * @see libs/model/ModelBehavior::beforeValidate()
-	 * 
+	 *
 	 * @return bool $success
 	 */
-	public function beforeValidate(&$model)
+	public function beforeValidate(Model $model)
 	{
 		parent::beforeValidate($model);
 
@@ -89,7 +89,7 @@ class PassValidatorBehavior extends ModelBehavior
 				return true;
 			}
 		}
-		
+
 		if(isset($this->model->data[$this->model->alias][$this->settings['fields']['password']]))
 		{
 			$pass = $this->model->data[$this->model->alias][$this->settings['fields']['password']];
@@ -111,64 +111,64 @@ class PassValidatorBehavior extends ModelBehavior
 			// seta um valor padrão
 			$confirm = null;
 		}
-		
+
 		// executa validação da senha
 		$success = $this->isValidPassword($pass, $confirm);
-		
+
 		// caso haja alguma falha
 		if($success !== true)
 		{
 			// adiciona os erros encontrados no atributo validationErrors do modelo atual
 			$this->model->validationErrors = array_merge($this->model->validationErrors, $success);
-			
+
 			// caso a configuração force a limpeza dos valores (senha e confirmação)
 			if($this->settings['unsetInFailure'])
 			{
 				if(isset($this->model->data[$this->model->alias][$this->settings['fields']['password']]))
 					unset($this->model->data[$this->model->alias][$this->settings['fields']['password']]);
-					
+
 				if(isset($this->model->data[$this->model->alias][$this->settings['fields']['confirm']]))
 					unset($this->model->data[$this->model->alias][$this->settings['fields']['confirm']]);
-			}	
+			}
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Método responsável pela execução da validação, baseada nas configurações do behavior
-	 * 
+	 *
 	 * @param string $pass string
 	 * @param string $confirm string
 	 */
 	public function isValidPassword($pass, $confirm = null)
 	{
 		$errors = array();
-		
+
 		// caso seja uma atualização do registro
 		if(isset($this->model->data[$this->model->alias]['id']) && empty($pass))
 		{
 			return true;
 		}
-		
+
 		// caso seja permitido não preencher o campo de senha
 		if($this->settings['allowEmpty'] === true && empty($pass))
 		{
 			return true;
 		}
-		
+
 		if(empty($pass))
 		{
 			$errors[$this->settings['fields']['password']] = $this->settings['errors']['required'];
 		}
 
 		$policyErrors = $this->validatePasswordPolicy($pass, $this->settings['fields']['password']);
-		
+
 		if($policyErrors !== true)
 		{
 			$errors = array_merge($errors, $policyErrors);
 		}
-		
+
 		// validações que dependem do campo de confirmação
 		if($this->settings['haveConfirm'])
 		{
@@ -177,7 +177,7 @@ class PassValidatorBehavior extends ModelBehavior
 			{
 				$errors[$this->settings['fields']['confirm']] = $this->settings['errors']['required'];
 			}
-			else 
+			else
 			{
 				//valida se a senha é igual a confirmação
 				if($pass != $confirm)
@@ -186,7 +186,7 @@ class PassValidatorBehavior extends ModelBehavior
 				}
 			}
 		}
-		
+
 		// caso não tenha sido encontrado nenhum erro
 		if(empty($errors))
 		{
@@ -207,13 +207,13 @@ class PassValidatorBehavior extends ModelBehavior
 	 * (ou seja, com os dados disponíveis em Model::data )
 	 *
 	 * @param array $conditions
-	 * 
+	 *
 	 * @return bool $success
 	 */
-	protected function evalConditions( $conditions )
+	protected function evalConditions($conditions)
 	{
 		$validOperators = array('and', 'or');
-		
+
 		foreach($conditions as $input => $value)
 		{
 			// possui subcondições
@@ -221,20 +221,20 @@ class PassValidatorBehavior extends ModelBehavior
 			{
 				// vetor com a avaliação de cada uma das condições
 				$statuses = array();
-				
+
 				$type = strtolower($input);
-				
+
 				// expressao em pre-ordem
 				if(in_array($type, $validOperators))
 				{
 					// inicialização do status final
 					if($type == 'or')
 						$final_status = false;
-				
+
 					// avalia cada uma das condições internas
 					foreach($value as $subinput => $subvalues)
 						$statuses[] = $this->evalConditions(array($subinput => $subvalues));
-						
+
 					// equaciona todas as respostas
 					foreach($statuses as $status)
 					{
@@ -243,7 +243,7 @@ class PassValidatorBehavior extends ModelBehavior
 						else if($status === false)
 							return false;
 					}
-					
+
 					return $final_status;
 				}
 				else
@@ -252,20 +252,20 @@ class PassValidatorBehavior extends ModelBehavior
 					foreach($value as $type => $subvalues)
 					{
 						$type = strtolower($type);
-						
+
 						if(!in_array($type, $validOperators))
 							continue;
-							
+
 						// inicialização do status final
 						if($type == 'or')
 							$final_status = false;
-							
+
 						// avalia cada uma das condições internas
 						foreach($subvalues as $subvalue)
 						{
 							$statuses[] = $this->evalConditions(array($input => $subvalue));
 						}
-							
+
 						// equaciona todas as respostas
 						foreach($statuses as $status)
 						{
@@ -274,14 +274,14 @@ class PassValidatorBehavior extends ModelBehavior
 							else if($status === false)
 								return false;
 						}
-						
+
 						return $final_status;
 					}
 				}
 			}
-			
+
 			$field = explode('.', $input);
-			
+
 			if(!isset($this->model->data[$field[0]][$field[1]]) || $this->model->data[$field[0]][$field[1]] != $value)
 			{
 				return false;
@@ -291,53 +291,53 @@ class PassValidatorBehavior extends ModelBehavior
 		return true;
 	}
 
-	
+
 	/**
 	 * Metodo responsavel pela validacao da politica de senha
 	 *
 	 * @param string $password senha a ser checada
 	 *
 	 * @return true caso a senha case com todos os requisitos da politica,
-	 *         array com os erros, caso contrario.	 
+	 *         array com os erros, caso contrario.
 	 *
 	 */
 	private function validatePasswordPolicy($password, $field)
 	{
 		$errors = array();
-		
+
 		// valida o tamanho da senha
 		if(mb_strlen($password) < $this->settings['minLength'])
 		{
 			$errors[$field] = $this->settings['errors']['minLength'];
 		}
-		
+
 		// valida o minimo de letras na senha
 		if($this->settings['minAlpha'] > 0)
 		{
 			$onlyLetters = mb_ereg_replace('[^a-z]', '', $password, 'i');
-			
+
 			if(mb_strlen($onlyLetters) < $this->settings['minAlpha'])
 			{
 				$errors[$field] = $this->settings['errors']['minAlpha'];
 			}
 		}
-		
+
 		// valida o nimimo de numeros na senha
 		if($this->settings['minNumbers'] > 0)
 		{
 			$onlyNumbers = mb_ereg_replace('[^0-9]', '', $password, 'i');
-			
+
 			if(mb_strlen($onlyNumbers) < $this->settings['minNumbers'])
 			{
 				$errors[$field] = $this->settings['errors']['minNumbers'];
 			}
 		}
-		
+
 		// valida o minimo de caracteres especiais na senha
 		if($this->settings['minSpecialChars'] > 0)
 		{
 			$onlySpecialChars = mb_ereg_replace('[a-z0-9]', '', $password, 'i');
-			
+
 			if(mb_strlen($onlySpecialChars) < $this->settings['minSpecialChars'])
 			{
 				$errors[$field] = $this->settings['errors']['minSpecialChars'];
@@ -349,12 +349,12 @@ class PassValidatorBehavior extends ModelBehavior
 		{
 			$errors[$field] = $this->settings['errors']['required'];
 		}
-		
+
 		if(empty($errors))
 		{
 			return true;
 		}
-		
+
 		return $errors;
 	}
 }
